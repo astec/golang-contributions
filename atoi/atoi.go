8c518c0dd176425484bf70ba9dc429b5f04b497c
Alexander Trakhimenok <astec@users.noreply.github.com>
2018-10-30 23:15:26 +0000

    improved atoi - DRY

--- atoi/atoi.go
+++ atoi/atoi.go
@@ -7,49 +7,35 @@ func AtoiImproved(s string) (int, error) {
 
 	sLen := len(s)
 
-	if strconv.IntSize == 32 && (0 < sLen && sLen < 10) ||
-		strconv.IntSize == 64 && (0 < sLen && sLen < 19) {
-		// Fast path for small integers that fit int type.
+	if sLen > 0 {
+		// Could be a fast path for small integers that fit int type.
+		first := 0
 		switch s[0] {
-		case '-':
+		// Check 1st char for sign.
+		case '+', '-':
 			if sLen == 1 {
-				return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
+				return 0, &NumError{fnAtoi, s, ErrSyntax}
 			}
+			first = 1
+		}
+
+		if intSize == 32 && sLen < 10 + first || intSize == 64 && sLen < 19 + first {
+			// Fast path for small integers that fit int type.
 			n := 0
-			for _, ch := range []byte(s[1:]) {
+			for _, ch := range s[first:] {
 				ch -= '0'
 				if ch > 9 {
-					return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
+					return 0, &NumError{fnAtoi, s, ErrSyntax}
 				}
 				n = n*10 + int(ch)
 			}
-			return -n, nil
-		case '+':
-			if sLen == 1 {
-				return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
-			}
-			n := 0
-			for _, ch := range []byte(s[1:]) {
-				ch -= '0'
-				if ch > 9 {
-					return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
-				}
-				n = n*10 + int(ch)
-			}
-			return n, nil
-		default:
-			n := 0
-			for _, ch := range []byte(s) {
-				ch -= '0'
-				if ch > 9 {
-					return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
-				}
-				n = n*10 + int(ch)
+			if s[0] == '-' {
+				n = -n
 			}
 			return n, nil
 		}
 	}
-
+		
 	// Slow path for invalid or big integers.
 	i64, err := strconv.ParseInt(s, 10, 0)
 	if nerr, ok := err.(*strconv.NumError); ok {
