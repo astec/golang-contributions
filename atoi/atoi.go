293cd259d03ae69d4f69cfa197fcf41cdcd7f457
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-10-31 00:12:45 +0000

    Now really improved

--- atoi/atoi.go
+++ atoi/atoi.go
@@ -9,35 +9,32 @@ func AtoiImproved(s string) (int, error) {
 
 	sLen := len(s)
 
-	if sLen > 0 {
-		// Could be a fast path for small integers that fit int type.
+	if intSize == 32 && 0 < sLen && sLen < 10 || intSize == 64 && 0 < sLen && sLen < 19 {
+		// Fast path for small integers that fit int type.
 		first := 0
-		switch s[0] {
-		// Check 1st char for sign.
-		case '+', '-':
-			if sLen == 1 {
+		neg := s[0] == '-'
+		if neg || s[0] == '+' {
+			// Check 1st char for sign.
+			// case '+', '-':
+			if sLen < 2 {
 				return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
 			}
 			first = 1
 		}
-
-		if intSize == 32 && sLen < 10 + first || intSize == 64 && sLen < 19 + first {
-			// Fast path for small integers that fit int type.
-			n := 0
-			for _, ch := range s[first:] {
-				ch -= '0'
-				if ch > 9 {
-					return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
-				}
-				n = n*10 + int(ch)
+		n := 0
+		for i := first; i < sLen; i++ {
+			ch := s[i] - '0'
+			if ch > 9 {
+				return 0, &strconv.NumError{fnAtoi, s, strconv.ErrSyntax}
 			}
-			if s[0] == '-' {
-				n = -n
-			}
-			return n, nil
+			n = n*10 + int(ch)
 		}
+		if neg {
+			n = -n
+		}
+		return n, nil
 	}
-		
+
 	// Slow path for invalid or big integers.
 	i64, err := strconv.ParseInt(s, 10, 0)
 	if nerr, ok := err.(*strconv.NumError); ok {
